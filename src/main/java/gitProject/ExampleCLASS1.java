6884bfa4f0e1896a5a79c8b0c7a44e934b53f0fe
dmiguelo <dmiguelo.pex@sanitas.es>
2020-04-04 20:54:30 +0200

    Commit 1 - MASTER

---
+++ src/main/java/gitProject/ExampleCLASS1.java
@@ -0,0 +1,11 @@
+package gitProject;
+
+
+public class ExampleCLASS1 {
+
+    public static void main( String[] args ) {
+        System.out.println( "Mensaje en main de MASTER" );
+    }
+    
+    
+}
