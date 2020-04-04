114763034b95b25fb2d97d2b5dde2d5a37c134e6
dmiguelo <dmiguelo.pex@sanitas.es>
2020-04-04 21:15:06 +0200

    Commit 2 RAMA_2

--- src/main/java/gitProject/ExampleCLASS1.java
+++ src/main/java/gitProject/ExampleCLASS1.java
@@ -7,10 +7,6 @@ public class ExampleCLASS1 {
         System.out.println( "Mensaje en main de MASTER" );
     }
     
-    public void metodo1RamaMASTER() {
-        System.out.println("metodo1RamaMASTER - Rama MASTER");
-    }
-
     public void metodo2RamaMASTER() {
         System.out.println("metodo2RamaMASTER - Rama RAMA_2");
     }
