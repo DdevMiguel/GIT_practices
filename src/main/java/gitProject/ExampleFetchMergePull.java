a7b96e75bc9b5d48f845e60839e7b8c951d0b449
dmiguelo <dmiguelo.pex@sanitas.es>
2020-04-09 19:39:58 +0200

    Commit 1 - feature/ExampleFetchMergePull_RAMA_2

--- src/main/java/gitProject/ExampleFetchMergePull.java
+++ src/main/java/gitProject/ExampleFetchMergePull.java
@@ -4,7 +4,11 @@ package gitProject;
 public class ExampleFetchMergePull {
 
     public static void main( String[] args ) {
-        System.out.println( "Mensaje original de EXAMPLEFETCHMERGEPULL" );
+        System.out.println( "Mensaje original de EXAMPLEFETCHMERGEPULL_RAMA_2" );
     }
 
+    public static void metodo1() {
+        System.out.println( "Mensaje metodo1 de EXAMPLEFETCHMERGEPULL_RAMA_2" );
+    }
+    
 }
