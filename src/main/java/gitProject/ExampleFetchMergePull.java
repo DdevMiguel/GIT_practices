edab99455f647ec20cd02d0448fdf7c4ca1e5ab1
dmiguelo <dmiguelo.pex@sanitas.es>
2020-04-09 19:37:31 +0200

    Commit 1 - MASTER - ExampleFetchMergePull

---
+++ src/main/java/gitProject/ExampleFetchMergePull.java
@@ -0,0 +1,10 @@
+package gitProject;
+
+
+public class ExampleFetchMergePull {
+
+    public static void main( String[] args ) {
+        System.out.println( "Mensaje original de EXAMPLEFETCHMERGEPULL" );
+    }
+
+}
