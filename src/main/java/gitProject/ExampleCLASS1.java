9b7e07f513e84168a62ff6c93dc8501e31a7827d
dmiguelo <dmiguelo.pex@sanitas.es>
2020-04-10 20:30:45 +0200

    Loúñtmoa

--- src/main/java/gitProject/ExampleCLASS1.java
+++ src/main/java/gitProject/ExampleCLASS1.java
@@ -4,11 +4,7 @@ package gitProject;
 public class ExampleCLASS1 {
 
     public static void main( String[] args ) {
-        System.out.println( "=====================================");
-        System.out.println( "=====================================");
-        System.out.println( "===========OTRA ACTUALZIACIÓN==========");
-        System.out.println( "=====================================");
-        System.out.println( "=====================================");
+        System.out.println( "===========YA NO QUEDA NADA==========");
     }
     
     
