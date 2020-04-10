951b985b76d81354f66739d4773755e141b84bd4
dmiguelo <dmiguelo.pex@sanitas.es>
2020-04-10 18:19:05 +0200

    Commit 2 - MASTER

--- src/main/java/gitProject/ExampleCLASS1.java
+++ src/main/java/gitProject/ExampleCLASS1.java
@@ -4,7 +4,7 @@ package gitProject;
 public class ExampleCLASS1 {
 
     public static void main( String[] args ) {
-        System.out.println( "Mensaje en main de MASTER" );
+        System.out.println( "Mensaje Original en MASTER" );
     }
     
     
