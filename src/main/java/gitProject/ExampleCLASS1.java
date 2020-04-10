da600ac21769d9343a3ec3d9bc85325081c53b2b
dmiguelo <dmiguelo.pex@sanitas.es>
2020-04-10 20:09:54 +0200

    Commit 3 - Rama 1 B

--- src/main/java/gitProject/ExampleCLASS1.java
+++ src/main/java/gitProject/ExampleCLASS1.java
@@ -4,15 +4,11 @@ package gitProject;
 public class ExampleCLASS1 {
 
     public static void main( String[] args ) {
-        System.out.println( "RAMA 1 B" );
-        System.out.println( "RAMA 1 B" );
-        System.out.println( " UPDATING" );
-        System.out.println( " UPDATING" );
-        System.out.println( " UPDATING" );
-        System.out.println( " UPDATING" );
-        System.out.println( " UPDATING" );
-        System.out.println( "RAMA 1 B" );
-        System.out.println( "RAMA 1 B" );
+        System.out.println( "=====================================");
+        System.out.println( "=====================================");
+        System.out.println( "===========OTRA ACTUALZIACIÓN==========");
+        System.out.println( "=====================================");
+        System.out.println( "=====================================");
     }
     
     
