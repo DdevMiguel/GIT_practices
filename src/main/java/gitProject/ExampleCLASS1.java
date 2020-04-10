ef5b5272b4f469b038caacb6c1998b2d7a7caf87
dmiguelo <dmiguelo.pex@sanitas.es>
2020-04-10 19:26:52 +0200

    Commit 1 - RAMA 1 B

--- src/main/java/gitProject/ExampleCLASS1.java
+++ src/main/java/gitProject/ExampleCLASS1.java
@@ -4,7 +4,16 @@ package gitProject;
 public class ExampleCLASS1 {
 
     public static void main( String[] args ) {
-        System.out.println( "MENSAJE EN ------------------------- rama 1" );
+        System.out.println( "RAMA 1 B" );
+        System.out.println( "RAMA 1 B" );
+        System.out.println( "RAMA 1 B" );
+        System.out.println( "RAMA 1 B" );
+        System.out.println( "RAMA 1 B" );
+        System.out.println( "RAMA 1 B" );
+        System.out.println( "RAMA 1 B" );
+        System.out.println( "RAMA 1 B" );
+        System.out.println( "RAMA 1 B" );
+        System.out.println( "RAMA 1 B" );
     }
     
     
