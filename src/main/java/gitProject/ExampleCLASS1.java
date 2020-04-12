b2df7edb49054447e5052418f34e260a449115a3
dmiguelo <dmiguelo.pex@sanitas.es>
2020-04-12 18:05:24 +0200

    Commit 1 - Rama 3 - Logado en Facebook

--- src/main/java/gitProject/ExampleCLASS1.java
+++ src/main/java/gitProject/ExampleCLASS1.java
@@ -6,6 +6,9 @@ public class ExampleCLASS1 {
     public static void main( String[] args ) {
         System.out.println( "Mensaje Original en MASTER" );
     }
-    
+ 
+    public void loginFacebook() {
+        System.out.println( "Logado en caralibro" );
+    }
     
 }
